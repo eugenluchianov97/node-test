66264a188cb01b013b65108d6bfb4c41ea6cba90
��eugen <evgenl1997@mail.ru>
2022-12-11 19:37:26 +0200

    olea commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-    res.send('Hello World22223333!')
+    res.send('Оля самая красивая девушка в мире :))!')
 })
 
 app.listen(port, () => {
